e948650ac5938f9f7014d5f26afde05666668291
CapSparkle <Feskovich.A@yandex.com>
2021-03-20 00:31:16 +0700

    Serializer Projected

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GreatWisdom;
+
+namespace GreatWisdom.DataBase
+{
+    public class JsonTools
+    {
+        //Serializer for simple 1 level depth types
+        public static string Serialize<T>(string toSerialize, T defeatedMonster) where T : new()
+        {
+            string prefix = "{ \"WisdomsList\":[";
+            //foreach (string wisdom in defeatedMonster.WisdomsList)
+            //    prefix += ( "\"" + wisdom + "\",");
+
+            string postfix = "}";
+
+            string serializingString = prefix + toSerialize + postfix;
+
+
+            //Check if DeserializeMonsterData works properly
+            if (Deserialize<T>(serializingString) != null)
+            {
+                
+            }
+            else
+            {
+                //Get data from Monster Data
+            }
+
+            //while writing serializingString for MonsterData
+            //if there is already serialized data for certain monster 
+            //=> deserialize it, summ with serializingString and serialize
+            
+            return null;
+        }
+
+        public static T Deserialize<T>(string toDeserialize) where T : new()
+        {
+            T t = new T();
+            return t;
+        }
+    }
+
+
+    public class MonsterData
+    {
+        public List<string> WisdomsList = new List<string>();
+        public List<string> WinningHeroesNames = new List<string>();
+        public string monsterName = "";
+    }
+    //winning sequence будет храниться отдельно
+
+
+    public class HeroData
+    {
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,41 +4,79 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using Json.Net;
+using Newtonsoft.Json;
 using System.Xml.Serialization;
 using System.IO;
 using System.Diagnostics;
+using GreatWisdom.DataBase;
 
-namespace IdealWorld
+namespace GreatWisdom
 {
+    class BaseClass
+    {
+        int number = 4;
+    }
+    class SomeClass
+    {
+        string un;
+        string wow = "hah";
+        private string rootFolderPath = @"C:\REPO\TvardLab\File_system\Peer's_files";
+        private string editorBatchFilePath = @"C:\REPO\TvardLab\File_system\Environment";
+
+        public SomeClass(string un)
+        {
+            this.un = un;
+            someDifficultLogic();
+        }
+
+        public SomeClass()
+        {
+            this.un = "lol";
+            someDifficultLogic();
+        }
+
+        public void someDifficultLogic()
+        {
+            Process writeFileProcess = new Process();
+            string editingFile = rootFolderPath + @"\" + @"Vasya" + @"\" + @"file1.txt";
+
+            writeFileProcess.StartInfo.WorkingDirectory = editorBatchFilePath;
+            writeFileProcess.StartInfo.FileName = editorBatchFilePath + @"\writeBatchFile.cmd";
+
+            writeFileProcess.StartInfo.Arguments = editingFile;
+            writeFileProcess.StartInfo.CreateNoWindow = true;
+            writeFileProcess.Start();
+            writeFileProcess.WaitForExit();
+        }
+    }
+
     class Program
     {
-        class SomeClass
-        {
-            string wow = "hah";
-            private string rootFolderPath = @"C:\REPO\TvardLab\File_system\Peer's_files";
-            private string editorBatchFilePath = @"C:\REPO\TvardLab\File_system\Environment";
-
-            public void someDifficultLogic()
-            {
-                Process writeFileProcess = new Process();
-                string editingFile = rootFolderPath + @"\" + @"Vasya" + @"\" + @"file1.txt";
-
-                writeFileProcess.StartInfo.WorkingDirectory = editorBatchFilePath;
-                writeFileProcess.StartInfo.FileName = editorBatchFilePath + @"\writeBatchFile.cmd";
-
-                writeFileProcess.StartInfo.Arguments = editingFile;
-                writeFileProcess.StartInfo.CreateNoWindow = true;
-                writeFileProcess.Start();
-                writeFileProcess.WaitForExit();
-            }
-        }
+        
 
 
         static void Main(string[] args)
         {
-            SomeClass sClass = new SomeClass();
-            string jsonString = JsonNet.Serialize(sClass);
+            SomeClass objectToSerialize = new SomeClass();
+
+            JsonParser parser = new JsonParser();
+
+            //энивевй какую бы строку ты не написал - можно мксимум изменить форму JSON ДОКУМЕНТА. Добавить доп поля. Можно сделать изменение поля по умолчанию
+            // what is default typeNamehandling?
+            string jsonString = JsonConvert.SerializeObject(objectToSerialize, Formatting.Indented, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.All
+            });
+
             Console.WriteLine(jsonString);
+
+
+            //BaseClass sClass = new BaseClass();
+            //string jsonString = JsonNet.Serialize(sClass);
+
+            //BaseClass newBuildedClass = JsonNet.Deserialize<BaseClass>("{\"number\":123,\"wow\":\"haha\"}");
+            
+            //Console.WriteLine(jsonString);
         }
     }
 }
